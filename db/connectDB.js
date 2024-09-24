540fd73cc5dd66581111912a1d2c495bdcb76985
chimcutdimua <kietle10085@gmail.com>
2024-09-24 09:51:57 +0700

    register user

--- db/connectDB.js
+++ db/connectDB.js
@@ -3,10 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
