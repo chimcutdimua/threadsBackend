04355f465495a0579ac20e3f19d42f91d7ed1f2a
chimcutdimua <kietle10085@gmail.com>
2024-09-24 10:06:49 +0700

    fix mongodb

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,9 +1,18 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+const mongoUrl = process.env.MONGO_URL;
+
+if (!mongoUrl) {
+  console.error("MONGO_URL is not defined in the environment variables");
+  process.exit(1);
+}
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
