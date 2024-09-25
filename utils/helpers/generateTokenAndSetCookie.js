57deafa2df26dff4df6f14434f27a07c1a6157b5
chimcutdimua <kietle10085@gmail.com>
2024-09-25 10:06:42 +0700

    fix

--- utils/helpers/generateTokenAndSetCookie.js
+++ utils/helpers/generateTokenAndSetCookie.js
@@ -10,7 +10,6 @@ const generateTokenAndSetCookie = (userID, res) => {
     httpOnly: true,
     maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days,
     secure: true,
-    sameSite: "none",
   });
 
   return token;
